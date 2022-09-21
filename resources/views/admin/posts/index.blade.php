4b1f6a62d23b85f2d388c6b7aa5fb96516cf3d5f
saff-it <saff_it@ymail.com>
2022-09-21 19:27:37 +0200

    attempt to foreign_key population

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -22,7 +22,7 @@
                     </a>
                     
                 </td>
-                <td>{{$post -> author}}</td>
+                <td>{{$post->user->name}}</td>
                 <td>{{$post -> post_date}}</td>
                 <td>{{$post -> post_content}}</td>
                 <td><button> <a href="{{route('admin.posts.edit', $post->id)}}"> Edit</a></button></td>
